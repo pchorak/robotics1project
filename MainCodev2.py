3936f29aabc006b528fed5fb6429cc4d88b1954c
Dylan <dellio15@gmail.com>
2016-11-28 17:47:33 -0500

    added arm calibration

--- MainCodev2.py
+++ MainCodev2.py
@@ -39,22 +39,22 @@ def move_xyz(interface, target):
     if any(np.isnan(angles)):
         print "Error: No solution for coordinates: ", target
     else:
-        interface.send_absolute_angles(angles[0],angles[1],angles[2],0.0) 
+        interface.send_absolute_angles(angles[0],angles[1],angles[2],0.0)
 
 
 
 # Touch the end effector to a detected object
 def touch(interface, tag_index, mode):
     angles = interface.current_status.angles[0:3]
-    
+
     # Get current XYZ
     P0t = DobotModel.forward_kinematics(angles)
-    
-    
+
+
     # Renew data
     data = camera.get_all_poses()[tag_index]
-    
-    # If arial mode and z is low, move up a bit  
+
+    # If arial mode and z is low, move up a bit
     # NO LONGER NEEDED
     if mode == 2 and P0t[2] < 0:
         P0t = P0t + [0,0,50]
@@ -66,50 +66,50 @@ def touch(interface, tag_index, mode):
     Rtc = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
     R0c = np.matmul(R0t, Rtc)
     Pta = np.matmul(R0c, data[0]) - np.matmul(R0c, Pct)
-    
+
     target = np.reshape(Pta, (3, 1)) + np.reshape(P0t, (3, 1))
-    
+
     # DIRECT MODE
     if mode == 1:
         # Move directly to target
-        move_xyz(interface, target)   
-        
+        move_xyz(interface, target)
+
     # ARIAL MODE
     elif mode == 2:
         # Move directly above target
         move_xyz(interface, target + np.reshape(np.array([0,0,50]) ,(3, 1)))
         # Move to target
-        move_xyz(interface, target)   
+        move_xyz(interface, target)
 
 
 
 # Track an AR Tag. DUCKY = 0  DUCKYBOT = 1   OBSTACLE = 2
-def track(interface, camera, tag_index):    
+def track(interface, camera, tag_index):
     # listen for user input
     req_exit = []
     listener = threading.Thread(target=input_thread, args=(req_exit,))
     listener.start()
-    
+
     time.sleep(1)
-    
+
     while not req_exit:
         # Only enter search if capture_data failed to find tag for 10 consecutive frames
         searching = True
         for x in range(0,30):
             time.sleep(0.1)
-            data = camera.get_all_poses()[tag_index]   
+            data = camera.get_all_poses()[tag_index]
             if data != [None, None]:
                 searching = False
                 break;
-        
+
         if searching:
             # Search until you find the desired tag
-            data = search(interface, camera, tag_index) 
-            
+            data = search(interface, camera, tag_index)
+
         # User requested to exit during search process
         if data == None:
             return
-        
+
         # Follow tag while it is in view
         while data != [None, None] and not req_exit:
                 # From kinematics
@@ -131,59 +131,34 @@ def track(interface, camera, tag_index):
                 if np.linalg.norm(correction) > 2.5:
                     angles = move_xyz(interface, P0t + correction)
                     time.sleep(0.5)
-                
+
                 # Get data
-                data = camera.get_all_poses()[tag_index] 
+                data = camera.get_all_poses()[tag_index]
 
 
 # Search for AR Tag. DUCKY = 0  DUCKYBOT = 1   OBSTACLE = 2
 # If tag_index is left undefined , it will search until any registered tag is found
 def search(interface, camera, tag_index = -1):
-    
+
     base_angle = interface.current_status.get_base_angle()
     direction = 1
-    
+
     # listen for user input
     req_exit = []
     listener = threading.Thread(target=input_thread, args=(req_exit,))
     listener.start()
-    
+
     while not req_exit:
         if direction == 1:
             while base_angle < 100 :
                 if req_exit:
                     return None
-                
-                # Get data
-                data = camera.get_all_poses()
-    
-                print data
-                
-                # Search all tags
-                if tag_index == -1:
-                    for x in range(0,3):
-                        if data[x] != [None, None]:
-                            return data[x]
-                else:
-                    # Search for a specified tag
-                    if data[tag_index] != [None, None]:
-                        return data[tag_index]
-                    
-                base_angle = base_angle + 10
-                
-                interface.send_absolute_angles(base_angle, 10, 10, 0)
-                
-                time.sleep(1)    
-        else:
-            while base_angle > -100 : 
-                if req_exit:
-                    return None                
 
                 # Get data
                 data = camera.get_all_poses()
-    
+
                 print data
-                
+
                 # Search all tags
                 if tag_index == -1:
                     for x in range(0,3):
@@ -193,20 +168,45 @@ def search(interface, camera, tag_index = -1):
                     # Search for a specified tag
                     if data[tag_index] != [None, None]:
                         return data[tag_index]
-                    
-                base_angle = base_angle - 10
-                
+
+                base_angle = base_angle + 10
+
                 interface.send_absolute_angles(base_angle, 10, 10, 0)
-                
+
                 time.sleep(1)
-            
+        else:
+            while base_angle > -100 :
+                if req_exit:
+                    return None
+
+                # Get data
+                data = camera.get_all_poses()
+
+                print data
+
+                # Search all tags
+                if tag_index == -1:
+                    for x in range(0,3):
+                        if data[x] != [None, None]:
+                            return data[x]
+                else:
+                    # Search for a specified tag
+                    if data[tag_index] != [None, None]:
+                        return data[tag_index]
+
+                base_angle = base_angle - 10
+
+                interface.send_absolute_angles(base_angle, 10, 10, 0)
+
+                time.sleep(1)
+
         # change direction
         direction = direction * -1
-        
+
     # user requested to exit
     interface.send_absolute_angles(10, 10, 10, 0)
     return None
-        
+
 
 # USED FOR GETTING INPUT TO BREAK LOOPS
 def input_thread(usr_list):
@@ -228,20 +228,20 @@ if __name__ == '__main__':
 
     # INITIALIZING CAMERA
     sys.stdout.write("Initiliazing the Camera..." )
-    
+
     camera = AR_Camera.Camera(CAMERA_ID, VIDEO_MODE, DUCKY, DUCKYBOT, OBSTACLE)
-    camera.start() 
-    
+    camera.start()
+
     print "OK!\n"
 
     # DISPLAY CAMERA PROPERTIES
     print camera
-    
+
     object_selection = '''OBJECTS:
     1. ducky
     2. duckybot
     3. obstacle
-    '''    
+    '''
 
     options = '''COMMANDS:
     reset
@@ -249,6 +249,7 @@ if __name__ == '__main__':
     get data
     touch
     track
+    arm calibration
     quit
     '''
     while True:
@@ -257,14 +258,30 @@ if __name__ == '__main__':
 
         if command == "reset":
             interface.send_absolute_angles(0,10,10,0)
+
+        elif command == "arm calibration":
+            angles = interface.current_status.angles[0:3]
+            # Get current XYZ
+            P0t = DobotModel.forward_kinematics(angles)
+            goal = [P0t[0],P0t[1], -20]
+            move_xyz(interface, goal)
+
+            time.sleep(5)
+
+            goal2 = np.reshape(goal ,(3, 1)) + np.reshape(np.array([0,150,0]) ,(3, 1))
+
+            move_xyz(interface, goal2)
+
+
+
         elif command == "top view":
             # Get current XYZ
-            P0t = DobotModel.forward_kinematics(interface.current_status.get_angles())    
+            P0t = DobotModel.forward_kinematics(interface.current_status.get_angles())
             P0t[2] = 100
             move_xyz(interface, P0t)
             # Allow the camera to refocus
-            time.sleep(2)            
-            
+            time.sleep(2)
+
         elif command == "get data":
             get_data(camera)
         elif command == "touch":
@@ -285,7 +302,7 @@ if __name__ == '__main__':
             selection = int(input("Which object to track: "))
             # Begin Tracking
             track(interface, camera, selection - 1)
-            
+
         elif command == "quit":
             sys.stdout.write("Releasing camera...")
             camera.release()
@@ -297,4 +314,3 @@ if __name__ == '__main__':
             interface.send_absolute_angles(0, 10, 10, 0)
             time.sleep(2)
             interface.send_absolute_angles(68, 10, 10, 0)
-            
