1c5455f9447bb7ab2af6384bdea647f262dfdcb1
pchorak <peter.c.horak@gmail.com>
2016-12-02 22:15:54 -0500

    added some plotting code to arm_cal_test

--- arm_cal_test.py
+++ arm_cal_test.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import DobotModel
 
-data = np.loadtxt('arm_cal.csv',delimiter=',')
+data = np.loadtxt('/Users/peter/Documents/MATLAB/robotics/arm_cal.csv',delimiter=',')
 angles = data[:,0:3]
 xy_ref = data[:,3:5]*(25.4/5)
 xy_arm = np.zeros(xy_ref.shape)
@@ -12,4 +13,16 @@ for k in range(xy_ref.shape[0]):
     p0t = DobotModel.forward_kinematics(angles[k,:])
     xy_arm[k,:] =  p0t[0:2] - xy0
 
-np.savetxt('data.csv',np.hstack((angles,xy_ref,xy_arm)),delimiter=',')
+np.savetxt('/Users/peter/Documents/MATLAB/robotics/data1.csv',np.hstack((angles,xy_ref,xy_arm)),delimiter=',')
+
+#plt.ion()
+#p = plt.plot(xy_ref[:,0],xy_ref[:,1],'+',xy_arm[:,0],xy_arm[:,1],'.')
+#plt.xlabel('X (mm)')
+#plt.ylabel('Y (mm)')
+#plt.legend(('Reference','Arm'))
+#plt.axis(np.array([-5,1,-5,5])*25.4)
+#plt.grid()
+#plt.xticks(np.unique(xy_ref[:,0]))
+#plt.yticks(np.unique(xy_ref[:,1]))
+
+
